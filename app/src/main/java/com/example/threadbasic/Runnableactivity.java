11708fdf22a2f7028c6d1f4ea998dc8bf75a5831
H-Subin <hsubin32@gmail.com>
2021-06-02 14:11:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/threadbasic/Runnableactivity.java
@@ -0,0 +1,61 @@
+package com.example.threadbasic;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class Runnableactivity extends AppCompatActivity {
+
+    Thread wr;
+    boolean running = true;
+    String TAG2 = "THREAD2";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        wr = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                for(i = 0; i < 20 && running; i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+
+                    }
+                    Log.v(TAG2, "Runnable time= " +i);
+                }
+            }
+        });
+        wr.start();
+        Log.v(TAG2, "Now I am in onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        running = false;
+        Log.v(TAG2, "Now I am in onStop");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG2, "Now I am in onPause");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG2, "Now I am in onResume");
+    }
+}
